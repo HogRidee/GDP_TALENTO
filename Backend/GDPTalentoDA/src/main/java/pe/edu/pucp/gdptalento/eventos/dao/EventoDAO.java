fc6c8d6068914fe7467034a8fe9046393adff7b0
fleurblau <dayana.arias@pucp.edu.pe>
2025-04-21 18:40:27 -0500

    Update EventoDAO.java

--- Backend/GDPTalentoDA/src/main/java/pe/edu/pucp/gdptalento/eventos/dao/EventoDAO.java
+++ Backend/GDPTalentoDA/src/main/java/pe/edu/pucp/gdptalento/eventos/dao/EventoDAO.java
@@ -3,11 +3,17 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package pe.edu.pucp.gdptalento.eventos.dao;
+import pe.edu.pucp.gdptalento.eventos.model.Evento;
+import java.util.ArrayList;
+
 
 /**
  *
  * @author USER
  */
 public interface EventoDAO {
-    
+    int insertar(Evento evento);
+    int modificar(Evento evento);
+    int eliminar(Evento evento);
+    ArrayList<Evento> listarTodos();
 }
