fd6bf4328f3c32c062eb735bc9d406e164f08356
fleurblau <dayana.arias@pucp.edu.pe>
2025-05-13 01:17:38 -0500

    daffd

---
+++ Backend/GDPTalentoDA/src/main/java/pe/edu/pucp/gdptalento/eventos/dao/AsistenciaDAO.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package pe.edu.pucp.gdptalento.eventos.dao;
+import java.util.ArrayList;
+import  pe.edu.pucp.gdptalento.eventos.model.Asistencia;
+import pe.edu.pucp.gdptalento.miembros.model.Staff;
+import pe.edu.pucp.gdptalento.eventos.model.Evento;
+
+/**
+ *
+ * @author USER
+ */
+public interface AsistenciaDAO{
+    
+    int insertar(Asistencia asistencia);
+    int modificar(Asistencia asistencia);
+    ArrayList<Asistencia> listarTodas ();      
+}
+
--- Backend/GDPTalentoDBManager/src/main/java/pucp/edu/pe/gdptalento/config/DBManager.java
+++ Backend/GDPTalentoDBManager/src/main/java/pucp/edu/pe/gdptalento/config/DBManager.java
@@ -1,30 +1,62 @@
 
 package pucp.edu.pe.gdptalento.config;
+
+import java.io.IOException;
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
 /**
  *
  * @author USER
  */
 public class DBManager {
-   private String hostname = "database-gamedevspucp-r.cl0s4cwya817.us-east-1.rds.amazonaws.com";
-   private String usuario = "admin";
-   private String password = "pv30g2sp1cDmaeu5404";
-   private String database = "gamedevspucpRecursosHumanos";
-   private String puerto = "3306";
-   private String url;
-   private Connection con;
+   //Patrón Singleton
+    private static DBManager dbManager;
+    
+    private Properties datos;
+    private final String hostname;
+    private final String usuario;
+    private final String password;
+    private final String database;
+    private final String puerto;
+    private final String url;
+    private Connection con;
+    private String rutaArchivo = "db.properties";
    
-    public DBManager() {
+    private DBManager(){
+        //Lectura del archivo properties
+        datos = new Properties();
+        try{
+            InputStream inputStream = getClass().getClassLoader().getResourceAsStream(rutaArchivo);
+            datos.load(inputStream);
+        }catch(IOException ex){
+            System.out.println(ex.getMessage());
+        }
+        //Asignamos valores del archivo leido
+        hostname = datos.getProperty("hostname");
+        usuario = datos.getProperty("usuario");
+        password = datos.getProperty("password");
+        puerto = datos.getProperty("puerto");
+        database = datos.getProperty("database");
+        //Formamos la URL de conexión        
         this.url = "jdbc:mysql://" + hostname + ":" + puerto + "/" + database;
-        
     }
-   
+    
+    public static DBManager getInstance(){
+        if(dbManager == null)
+            dbManager = new DBManager();
+        return dbManager;
+    }
+    
+    //Nos permite obtener una conexión con la BD
     public Connection getConnection(){
         try{
             Class.forName("com.mysql.cj.jdbc.Driver");
-            con = DriverManager.getConnection(url,usuario,password);
-        }catch(Exception ex){
+            con = DriverManager.getConnection(url, usuario, password);
+            System.out.println("Se ha establecido la conexion con la BD...");
+        }catch(ClassNotFoundException | SQLException ex){
             System.out.println(ex.getMessage());
         }
         return con;
