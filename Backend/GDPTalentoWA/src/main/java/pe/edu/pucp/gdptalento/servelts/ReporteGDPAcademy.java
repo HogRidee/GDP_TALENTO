8f3025fca818cb2b2ff3014d5ac12bddd6a3485e
AaronSH123 <163920863+AaronSH123@users.noreply.github.com>
2025-06-23 01:25:34 -0500

    OIE and areas Report

---
+++ Backend/GDPTalentoWA/src/main/java/pe/edu/pucp/gdptalento/servelts/ReporteGDPAcademy.java
@@ -0,0 +1,75 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+package pe.edu.pucp.gdptalento.servelts;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.awt.Image;
+import java.net.URL;
+import java.net.URLDecoder;
+import java.sql.Connection;
+import java.util.HashMap;
+import javax.swing.ImageIcon;
+import net.sf.jasperreports.engine.JREmptyDataSource;
+import net.sf.jasperreports.engine.JRException;
+import net.sf.jasperreports.engine.JasperExportManager;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.engine.JasperReport;
+import net.sf.jasperreports.engine.util.JRLoader;
+import pucp.edu.pe.gdptalento.config.DBManager;
+
+/**
+ *
+ * @author USER
+ */
+public class ReporteGDPAcademy extends HttpServlet {
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        try{
+            //Referenciamos el archivo Jasper
+            JasperReport jr = (JasperReport) JRLoader.loadObject(getClass().getResourceAsStream("/pe/edu/pucp/gdptalento/reportes/ReporteGDPAcademy.jasper"));
+            //Establecemos los parametros que necesita el reporte
+            HashMap parametros = new HashMap();
+            //Referenciamos la imagen del logo y los subreportes
+            URL rutaLogo = getClass().getResource("/pe/edu/pucp/gdptalento/reportes/LOGO_GDP.png");
+            //Generamos los objetos necesarios en el reporte
+            Image logo = (new ImageIcon(rutaLogo)).getImage();
+            //Colocamos los parámetros
+            parametros.put("logo", logo);
+            //Establecemos la conexión
+            Connection con = DBManager.getInstance().getConnection();
+            //Poblamos el reporte
+            JasperPrint jp = JasperFillManager.fillReport(jr, parametros, con);
+            //Mostramos por pantalla
+            JasperExportManager.exportReportToPdfStream(jp, response.getOutputStream());
+        }catch(Exception ex){
+            System.out.println(ex.getMessage());
+        }finally{
+            DBManager.getInstance().cerrarConexion();
+        }
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }
+
+}
--- SQL/Test Procedure.sql
+++ SQL/Test Procedure.sql
@@ -18,3 +18,91 @@ Call listar_Evento();
 Call LISTAR_ROLES();
 
 call LISTAR_TAREA();
+
+Call LISTAR_TAREA_ENCARGADO();
+
+call LISTAR_POSTULANTES();
+
+
+SELECT 
+    m.codigoPUCP,
+    m.nombre,
+    m.telefono,
+    m.correo,
+    m.especialidad,
+    s.area,
+    s.fecha_ingreso,
+    s.desempenio
+FROM Staff s
+JOIN MiembroPUCP m ON s.id_staff = m.id_miembro_pucp
+WHERE s.estado = 'ACTIVO' and s.area='MARKETING';
+
+
+SELECT 
+    m.codigoPUCP,
+    m.nombre,
+    m.telefono,
+    m.correo,
+    m.especialidad,
+    s.area,
+    s.fecha_ingreso,
+    s.desempenio
+FROM Staff s
+JOIN MiembroPUCP m ON s.id_staff = m.id_miembro_pucp
+WHERE s.estado = 'ACTIVO' and s.area='RECURSOS_HUMANOS';
+
+
+SELECT 
+    m.codigoPUCP,
+    m.nombre,
+    m.telefono,
+    m.correo,
+    m.especialidad,
+    s.area,
+    s.fecha_ingreso,
+    s.desempenio
+FROM Staff s
+JOIN MiembroPUCP m ON s.id_staff = m.id_miembro_pucp
+WHERE s.estado = 'ACTIVO' and s.area='PRESIDENCIA';
+
+SELECT 
+    m.codigoPUCP,
+    m.nombre,
+    m.telefono,
+    m.correo,
+    m.especialidad,
+    s.area,
+    s.fecha_ingreso,
+    s.desempenio
+FROM Staff s
+JOIN MiembroPUCP m ON s.id_staff = m.id_miembro_pucp
+WHERE s.estado = 'ACTIVO' and s.area='GDP_ACADEMY';
+
+SELECT 
+    m.codigoPUCP,
+    m.nombre,
+    m.telefono,
+    m.correo,
+    m.especialidad,
+    s.area,
+    s.fecha_ingreso,
+    s.desempenio
+FROM Staff s
+JOIN MiembroPUCP m ON s.id_staff = m.id_miembro_pucp
+WHERE s.estado = 'ACTIVO' and s.area='EVENTOS';
+
+
+SELECT 
+    mp.nombre AS "Nombres y apellidos",
+    mp.codigoPUCP AS "Código PUCP",
+    mp.facultad AS "Facultad",
+    mp.especialidad AS "Especialidad",
+    mp.status AS "Status 2025-1 (matriculado, no-matriculado, egresado, externo)",
+    'Game Devs PUCP' AS "Agrupación"
+FROM Staff s
+JOIN MiembroPUCP mp ON s.id_staff = mp.id_miembro_pucp
+WHERE s.estado = 'ACTIVO';
+
+UPDATE Postulante SET estado_proceso = 'PENDIENTE' WHERE id_postulante = 10;
+UPDATE Postulante SET estado_proceso = 'APROBADO' WHERE id_postulante = 11;
+UPDATE Postulante SET estado_proceso = 'RECHAZADO' WHERE id_postulante = 12;
